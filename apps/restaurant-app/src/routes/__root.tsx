209b3d1f90a82535056af9b2cb47b2610af28c21
Matias Barro <matias.barro@gmail.com>
2024-05-25 19:29:22 -0300

    Use RestaurantList component in Index page

--- apps/restaurant-app/src/routes/__root.tsx
+++ apps/restaurant-app/src/routes/__root.tsx
@@ -1,13 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import NavigationBar from '@/components/common/NavigationBar';
-import { Home } from 'lucide-react';
 
 export const Route = createRootRoute({
     component: () => (
         <div className="w-full min-h-screen">
             <NavigationBar />
-            <div className="container mx-auto p-4">
+            <div className="p-4">
                 <Outlet />
             </div>
             <TanStackRouterDevtools />
--- apps/restaurant-app/src/routes/index.lazy.tsx
+++ apps/restaurant-app/src/routes/index.lazy.tsx
@@ -1,9 +1,26 @@
+import RestaurantList from '@/components/restaurants/RestaurantList';
+import { TypographyH3 } from '@/components/ui/typographyH3';
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { Restaurant } from 'restaurant-types';
+import logo from '@/assets/restaurant-logo.png';
 
 export const Route = createLazyFileRoute('/')({
     component: Index,
 });
 
+const restaurants: Restaurant[] = [
+    { name: 'Restaurant 1', address: '123 ASDF', id: '1', image: logo },
+    { name: 'Restaurant 2', address: '123 ASDF', id: '2', image: logo },
+    { name: 'Restaurant 3', address: '123 ASDF', id: '3', image: logo },
+    { name: 'Restaurant 4', address: '123 ASDF', id: '4', image: logo },
+    { name: 'Restaurant 5', address: '123 ASDF', id: '5', image: logo },
+];
+
 function Index() {
-    return <h3>Welcome Home!</h3>;
+    return (
+        <div className="flex flex-col gap-4">
+            <TypographyH3>Restaurants</TypographyH3>
+            <RestaurantList restaurants={restaurants} />
+        </div>
+    );
 }
