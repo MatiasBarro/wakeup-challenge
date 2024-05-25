b118bb6bc53f4b528750621009b32efef1367a09
Matias Barro <matias.barro@gmail.com>
2024-05-25 19:28:39 -0300

    Add RestaurantList component

---
+++ apps/restaurant-app/src/components/restaurants/RestaurantList/RestaurantList.tsx
@@ -0,0 +1,19 @@
+import { Restaurant } from 'restaurant-types';
+import RestaurantItem from '../RestaurantItem';
+
+type RestaurantListProps = {
+    restaurants: Restaurant[];
+};
+
+export function RestaurantList({ restaurants }: RestaurantListProps) {
+    return (
+        <div className="flex flex-wrap gap-y-8 justify-between">
+            {restaurants.map((restaurant) => (
+                <RestaurantItem
+                    restaurant={restaurant}
+                    className="w-full lg:w-[calc(50%-1rem)]"
+                />
+            ))}
+        </div>
+    );
+}
