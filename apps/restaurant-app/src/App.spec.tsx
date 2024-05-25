7247824ea5e5d6b353129c682aa9def91ef33ab5
Matias Barro <matias.barro@gmail.com>
2024-05-25 13:24:37 -0300

    Setup restaurant app test

---
+++ apps/restaurant-app/src/App.spec.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+    it('renders the App component', () => {
+        render(<App />);
+    });
+
+    it('should render title', () => {
+        render(<App />);
+        screen.getByText('Vite + React');
+    });
+});
