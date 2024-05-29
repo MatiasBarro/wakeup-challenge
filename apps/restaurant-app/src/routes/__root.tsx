10c2068ab575c4dee5aa3e9b9cfe8fb9e557af7f
Matias Barro <matias.barro@gmail.com>
2024-05-29 19:18:31 -0300

    Catch page errors with default router error

--- apps/restaurant-app/src/routes/__root.tsx
+++ apps/restaurant-app/src/routes/__root.tsx
@@ -1,9 +1,16 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import {
+    createRootRoute,
+    ErrorComponent,
+    Outlet,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import NavigationBar from '@/components/common/NavigationBar';
 import { Toaster } from '@/components/ui/toaster';
 
 export const Route = createRootRoute({
+    errorComponent: ({ error }) => {
+        return <ErrorComponent error={error} />;
+    },
     component: () => (
         <div className="w-full min-h-screen">
             <NavigationBar />
