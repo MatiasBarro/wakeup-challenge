e4defea6c00927f28fbc5f4097d290680cc9b252
Matias Barro <matias.barro@gmail.com>
2024-05-28 23:39:14 -0300

    Display restaurant data in restaurant section

--- apps/restaurant-app/src/routes/restaurant.$restaurantId.tsx
+++ apps/restaurant-app/src/routes/restaurant.$restaurantId.tsx
@@ -1,29 +1,38 @@
-import { fetchRestaurantProducts } from '@/api';
+import { useEffect } from 'react';
+import { Product } from 'restaurant-types';
+import { fetchRestaurantById } from '@/api';
 import OrderDrawer from '@/components/restaurants/OrderDrawer';
 import ProductList from '@/components/restaurants/ProductList';
 import { TypographyH3 } from '@/components/ui/typographyH3';
 import { useOrderStore } from '@/stores/useOrderStore';
 import { createFileRoute, useLocation } from '@tanstack/react-router';
-import { useEffect } from 'react';
-import { Product } from 'restaurant-types';
 
 export const Route = createFileRoute('/restaurant/$restaurantId')({
     component: () => <Restaurant />,
     loader: async ({ params: { restaurantId } }) => {
-        const products = await fetchRestaurantProducts(restaurantId);
-        return products.reduce(
-            (acc, product) => {
-                acc[product.id] = product;
-                return acc;
-            },
-            {} as Record<string, Product>,
-        );
+        const { products, ...restaurant } =
+            await fetchRestaurantById(restaurantId);
+
+        const productsMap = products
+            ? products?.reduce(
+                  (acc, product) => {
+                      acc[product.id] = product;
+                      return acc;
+                  },
+                  {} as Record<string, Product>,
+              )
+            : {};
+
+        return {
+            restaurant,
+            products: productsMap,
+        };
     },
 });
 
 export function Restaurant() {
     const location = useLocation();
-    const products = Route.useLoaderData();
+    const { restaurant, products } = Route.useLoaderData();
     const resetOrderState = useOrderStore((state) => state.resetState);
 
     useEffect(() => {
@@ -33,7 +42,10 @@ export function Restaurant() {
     return (
         <div className="flex flex-col gap-6">
             <div className="flex flex-row justify-between">
-                <TypographyH3>SuperSano</TypographyH3>
+                <div className="flex flex-col">
+                    <TypographyH3>{restaurant.name}</TypographyH3>
+                    <span>{restaurant.address}</span>
+                </div>
                 <OrderDrawer products={products} />
             </div>
             <ProductList products={Object.values(products)} />
